0f3a6a6468bc3b37bc531a4f7bc5c1772bda81ad
ThePittt <150543731+ThePittt@users.noreply.github.com>
2023-11-11 18:18:34 +0000

    Update FlyingEnemy.cs

--- Return to the grave/Assets/Scripts/FlyingEnemy.cs
+++ Return to the grave/Assets/Scripts/FlyingEnemy.cs
@@ -6,42 +6,69 @@ public class FlyingEnemy : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D enemyRigidbody;
     [SerializeField] private Transform player;
-    public float patrolHeight = 5f;
-    public float patrolSpeed = 2f;
-    public float detectionRange = 5f;
-    public float followSpeed = 4f;
 
+    private float patrolHeight = 5f;
+    private float patrolWidth = 5f;
+    private float patrolSpeed = 2f;
+    private float descendSpeed = 2f;
+    private float detectionRange = 5f;
+    private float followSpeed = 4f;
+
+    private bool isPatrolling = true;
     private Vector2 initialPosition;
 
-     private void Start()
+    private void Start()
     {
         initialPosition = transform.position;
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-   void Update()
-{
-    if (Vector2.Distance(transform.position, player.position) < detectionRange)
+    private void Update()
     {
-        FollowPlayer();
+        if (isPatrolling)
+        {
+            Patrol();
+        }
+        else
+        {
+            Descend();
+            FollowPlayer();
+        }
     }
-    else
+
+    private void Patrol()
     {
-        Patrol();
+        float horizontalStep = patrolSpeed * Time.deltaTime;
+        float verticalStep = Mathf.Sin(Time.time * patrolSpeed) * patrolHeight * Time.deltaTime;
+
+        transform.Translate(new Vector2(horizontalStep, verticalStep));
+
+        if (Vector2.Distance(transform.position, player.position) < detectionRange)
+        {
+            isPatrolling = false;
+        }
+
+        if (Mathf.Abs(transform.position.x - initialPosition.x) >= patrolWidth)
+        {
+            // Change direction when reaching the patrol width limit
+            patrolSpeed *= -1;
+        }
     }
-}
 
-void Patrol()
-{
-    float verticalMovement = Mathf.Sin(Time.time * patrolSpeed) * patrolHeight * Time.deltaTime;
-    enemyRigidbody.velocity = new Vector2(0f, verticalMovement);
-}
+    private void Descend()
+    {
+        float step = descendSpeed * Time.deltaTime;
+        transform.Translate(Vector2.down * step);
+    }
 
-void FollowPlayer()
-{
-    float step = followSpeed * Time.deltaTime;
-    enemyRigidbody.position = Vector2.MoveTowards(enemyRigidbody.position, player.position, step);
-}
+   private void FollowPlayer()
+    {
+        float step = followSpeed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, player.position, step);
 
-
+        if (Vector2.Distance(transform.position, player.position) > detectionRange)
+        {
+            isPatrolling = true;
+        }
+    }
 }
